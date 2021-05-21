8a410ae48955b5251659c9e989c2307839ca25c7
Sergey <kurilovsergey15@gmail.com>
2021-05-22 00:39:09 +0300

    config

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+module.exports = {
+    images: {
+        domains: ["localhost", "nextjs-calendar-jtyqtqju4-sergeykurilov.vercel.app"],
+        path: 'http://nextjs-calendar-jtyqtqju4-sergeykurilov.vercel.app'
+    }
+};
