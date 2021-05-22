193b118981c1882f4bbd65705ba1908bcf84ab84
Sergey <kurilovsergey15@gmail.com>
2021-05-22 20:46:07 +0300

    week day modal fix

--- components/Modal/MyModal.tsx
+++ components/Modal/MyModal.tsx
@@ -3,10 +3,13 @@ import {TransitionGroup, CSSTransition} from "react-transition-group";
 import {ModalType} from "../../interfaces";
 
 function Modal(props: ModalType) {
+    console.log(String(props.selectedDate).substr(0,3))
+
     const selectedMonth = props.selectedDate?.getMonth();
     const selectedDate = props.selectedDate?.getDate();
     const selectedYear = props.selectedDate?.getFullYear();
     const arr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const weekDayNames = new Date(props.selectedDate).toLocaleString('en-us', {weekday:'long'})
     return (
         <TransitionGroup className="modal-wrapper">
             {props.showModal && (
@@ -25,6 +28,11 @@ function Modal(props: ModalType) {
                             <div>
                                 <label className="modal_label">Date <input onBlur={props.closeModal} className="modal_input" type="text"
                                                                            value={selectedDate} onChange={() => {
+                                }}/></label>
+                            </div>
+                            <div>
+                                <label className="modal_label">Week Day <input onBlur={props.closeModal} className="modal_input" type="text"
+                                                                           value={weekDayNames} onChange={() => {
                                 }}/></label>
                             </div>
                             <div>
