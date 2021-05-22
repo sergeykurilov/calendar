6e32e629a45cd7bf4c04d5c8cd693144a1fd5789
Sergey <kurilovsergey15@gmail.com>
2021-05-22 20:35:43 +0300

    fix main image

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-module.exports = {
-    images: {
-        domains: ["localhost", "nextjs-calendar-jtyqtqju4-sergeykurilov.vercel.app"],
-        path: 'http://nextjs-calendar-jtyqtqju4-sergeykurilov.vercel.app'
-    }
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,6 @@ const IndexPage = () => {
             <section className={classes.main}>
                 <div className={classes.mainBlocks}>
                     <div className={classes.content}>
-                        <img className={classes.image} style={{width: "700px", height: "475px"}}
-                             alt="Mountains"  src="/images/artboard.png"/>
                         <div className="text-content">
                             <h1>Choose the day for the meeting</h1>
                             <p>We encourage you to book your appointment online. This will save you time.</p>
